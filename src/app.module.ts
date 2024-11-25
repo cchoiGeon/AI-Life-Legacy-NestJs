79ff5d21a72a689d8104b5614c1ae12f66554e70
choigeon <qrwrqrwrer@naver.com>
2024-11-25 16:52:29 +0900

    feat: mysql과 연동 후 typeorm 이용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { ChatgptModule } from './chatgpt/chatgpt.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [ChatgptModule,ConfigModule.forRoot({isGlobal:true})],
+  imports: [
+    ChatgptModule,ConfigModule.
+    forRoot({
+      isGlobal:true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as any,
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User],
+        synchronize: configService.get<boolean>('DB_SYNC'),
+      }),
+    }),
+    UserModule,
+  ],
 })
 
 export class AppModule {}
